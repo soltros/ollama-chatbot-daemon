ea11f5bf131dd36e97eec670e0db2981771993f6
Derrik Diener <142091676+soltros@users.noreply.github.com>
2023-12-15 16:05:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ## Download the Chatbot Ollama tool ##
 git clone https://github.com/ivanfioravanti/chatbot-ollama.git
 cd chatbot-ollama/
+npm ci
 
 ## Download the Ollama Chatbot Daemon ##
 curl https://raw.githubusercontent.com/soltros/ollama-chatbot-daemon/main/daemon.py -o daemon.py
