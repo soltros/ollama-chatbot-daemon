75a88a406dfcf715b336afe51cbc46d1a13b28a8
Derrik Diener <142091676+soltros@users.noreply.github.com>
2023-12-15 15:56:26 -0500

    Update daemon.py

--- daemon.py
+++ daemon.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
 
 import subprocess
-import time
+import threading
+import sys
 
-# Run npm run dev in the background
-proc = subprocess.Popen(['npm', 'run', 'dev'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+def read_output(process):
+    while True:
+        output = process.stdout.readline()
+        if process.poll() is not None and output == '':
+            break
+        if output:
+            print(output.strip().decode())
 
-# Get the process ID of the running process (optional, as it's not used here)
-pid = proc.pid
+def run_npm():
+    process = subprocess.Popen(['npm', 'run', 'dev'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
-# Wait for a key press to terminate the program
-input("Press enter to stop the program: ")
+    # Start a thread to read and print output
+    output_thread = threading.Thread(target=read_output, args=(process,))
+    output_thread.start()
 
-# Terminate the program using the process ID
-proc.kill()
-proc.wait()  # Wait for process to terminate completely
+    # Wait for a key press to terminate the program
+    input("Press enter to stop the program: ")
+
+    # Terminate the npm process
+    process.kill()
+    process.wait()  # Wait for the process to terminate completely
+
+    # Wait for the output thread to finish
+    output_thread.join()
+
+if __name__ == "__main__":
+    run_npm()
