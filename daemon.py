0c8bbd71ae3ed526b9e3097e79a29b93ec949f1b
Derrik Diener <142091676+soltros@users.noreply.github.com>
2023-12-15 15:48:24 -0500

    Update daemon.py

--- daemon.py
+++ daemon.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
+
 import subprocess
 import time
 
-# Run npm run dev in the background and disconnect from the terminal
+# Run npm run dev in the background
 proc = subprocess.Popen(['npm', 'run', 'dev'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-disown
 
-# Get the process ID of the running process
+# Get the process ID of the running process (optional, as it's not used here)
 pid = proc.pid
 
 # Wait for a key press to terminate the program
@@ -13,3 +14,4 @@ input("Press enter to stop the program: ")
 
 # Terminate the program using the process ID
 proc.kill()
+proc.wait()  # Wait for process to terminate completely
